25c5cde3651fa92729ef4fbe676f39652b720a66
Renato Alves de Sousa <renato.alvs1103@gmail.com>
2025-10-03 12:52:36 -0300

    Add files via upload

---
+++ data_processing/view_parquet.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+parqs = ['A','B','C','D','E']
+
+for i in range(len(parqs)):
+    print('Parquet: ' + parqs[i])
+    df = pd.read_parquet('data/parquets/G1-2023-02-21-' + parqs[i] + '.parquet')
+
+    print(df.columns)
+    print(df)
+    print('\n')
