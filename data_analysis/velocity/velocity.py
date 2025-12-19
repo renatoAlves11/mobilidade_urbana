aec94de0a906ee275a56e2a495794f78084f1e81
renato-alvs <renato.alvs1103@gmail.com>
2025-12-19 01:10:06 -0300

    Update velocity.py

--- data_analysis/velocity/velocity.py
+++ data_analysis/velocity/velocity.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import glob
 
-# === Carregar todos os arquivos RData ===
+# === Carregar todos os arquivos CSV ===
 arquivos = glob.glob("../../data/bus_csv/sul/LINHA_*_COMPLETO.csv")  # pega todos os arquivos no padrão
 
 dfs = []
@@ -13,7 +13,6 @@ for arquivo in arquivos:
 
     df = df[["GPSTIMESTAMP", "LINE", "VELOCITY", "PARKING"]].dropna(subset=["GPSTIMESTAMP", "LINE", "VELOCITY"])
     df = df[df["PARKING"].isna()]
-    #df = df[df["VELOCITY"] > 0]
 
     # Converte timestamp
     df["GPSTIMESTAMP"] = pd.to_datetime(df["GPSTIMESTAMP"], errors="coerce")
@@ -26,23 +25,29 @@ for arquivo in arquivos:
     dfs.append(df)
     dfs1.append(df1)
 
-
 # Junta tudo em um único DataFrame
 dados = pd.concat(dfs, ignore_index=True)
 dados1 = pd.concat(dfs1, ignore_index=True)
 
 print("Dados carregados:", dados["LINE"].unique())
 
+# === Filtragem: remover linhas com poucos registros ===
+n_min = 1000  # mínimo de registros para considerar a linha
+linhas_validas = dados['LINE'].value_counts()[dados['LINE'].value_counts() >= n_min].index
+
+dados_filtrados = dados[dados['LINE'].isin(linhas_validas)]
+dados1_filtrados = dados1[dados1['LINE'].isin(linhas_validas)]
+
 #--------------------------------------
 # Velocidade média por hora do dia
-vel_por_hora = dados.groupby(["LINE", "HORA"])["VELOCITY"].mean().reset_index()
+vel_por_hora = dados_filtrados.groupby(["LINE", "HORA"])["VELOCITY"].mean().reset_index()
 
 # Paleta de cores contrastantes
 colors = ["#E63946", "#2A9D8F", "#457B9D", "#F4A261", "#1D3557"]
 
 # === Plot 1A: Velocidade até meio-dia === 
 plt.figure(figsize=(10,6)) 
-sns.lineplot( data=vel_por_hora, x="HORA", y="VELOCITY", hue="LINE", marker="o", palette=colors ) 
+sns.lineplot(data=vel_por_hora, x="HORA", y="VELOCITY", hue="LINE", marker="o", palette=colors) 
 plt.title("Velocidade média por hora do dia (0h – 11h)")
 plt.xlabel("Hora do dia") 
 plt.ylabel("Velocidade média (km/h)") 
@@ -50,19 +55,18 @@ plt.legend(title="Linha")
 plt.tight_layout() 
 plt.show()
 
-
-# # === Plot 2: Distribuição da velocidade por linha (Boxplot) ===
+# === Plot 2: Distribuição da velocidade por linha (Boxplot) ===
 plt.figure(figsize=(8,6))
-sns.boxplot(data=dados, x="LINE", y="VELOCITY", hue = 'LINE', palette=colors)
+sns.boxplot(data=dados_filtrados, x="LINE", y="VELOCITY", hue='LINE', palette=colors)
 plt.title("Distribuição da velocidade por linha de ônibus")
 plt.xlabel("Linha")
 plt.ylabel("Velocidade (km/h)")
 plt.tight_layout()
 plt.show()
 
-# # === Plot 3: Histograma de velocidades por linha ===
+# === Plot 3: Histograma de velocidades por linha ===
 plt.figure(figsize=(10,6))
-sns.histplot(data=dados1, x="VELOCITY", hue="LINE", kde=True, element="step", common_norm=False, palette=colors)
+sns.histplot(data=dados1_filtrados, x="VELOCITY", hue="LINE", kde=True, element="step", common_norm=False, palette=colors)
 plt.title("Distribuição de frequências de velocidade por linha")
 plt.xlabel("Velocidade (km/h)")
 plt.ylabel("Frequência")
